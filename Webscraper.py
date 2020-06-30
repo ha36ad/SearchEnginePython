d27e779b78ec7b4c1b2d57ad7e81aa369361049a
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-06-29 22:11:02 -0400

    Update Webscraper.py

--- Webscraper.py
+++ Webscraper.py
@@ -3,4 +3,13 @@ from bs4 import BeautifulSoup
 from csv import writer
 from urllib.request import urlopen as uReq 
 
-define main():
+my_url = "http://quotes.toscrape.com/page/2/"
+Client = uReq(my_url)
+page_scrape_html= Client.read()
+Client.close()
+
+
+soup= BeautifulSoup(html_doc, 'html.parser')
+
+soupfind =soup.findAll ('div')
+
