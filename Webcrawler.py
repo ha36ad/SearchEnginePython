9af1e80b646944fb6dec19ecabbd5aac7b0d6d7f
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-02 21:29:43 -0400

    Update Webcrawler.py

--- Webcrawler.py
+++ Webcrawler.py
@@ -1,1 +1,29 @@
 import scrapy
+from pathlib import Path
+
+#project path input
+project_path = input("Enter your path (C:, D:, etc.) : ")
+#Storing user's path
+path = Path(project_path)
+#URL input
+my_url = input("Enter the url you are crawling: ")
+
+# Function for creating a  file
+def write_file(path, data):
+    with open (path,"w") as file:
+        file.write(data)
+        
+#Create queue and crawl files
+def create_queue(project_path, url):
+    queue =  path / "queue.txt"
+    crawl =  path / "crawl.txt"
+    
+    #Writing  the files
+    write_file(queue, url)
+    write_file(crawl,  " ")    
+
+create_queue(path , my_url)   
+
+
+
+
