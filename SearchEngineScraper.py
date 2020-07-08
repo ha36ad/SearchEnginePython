746dbbdf3c2acce09c97f748facebdbf78c3f616
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-07 23:17:35 -0400

    Update SearchEngineScraper.py

--- SearchEngineScraper.py
+++ SearchEngineScraper.py
@@ -12,19 +12,23 @@ d_search = DuckSearch()
 #Search prompt
 search_query = input("Enter your search query here: ") 
 search_set = (search_query, 1)
+search_set2 =(search_query, 2)
 
 #Searching for query
 g_results = g_search.search(*search_set)
-#b_results = b_search.search(*search_set)
-#d_results = d_search.search(*search_set)
+b_results = b_search.search(*search_set2)
+d_results = d_search.search(*search_set2)
 y_results = y_search.search(*search_set)
 
 # print first title from google search
 for i in range(0,5):
     if g_results["links"] == y_results["links"]:
-       print("")
+        print(b_results["links"][i])
+        print(d_results ["links"][i])
+        print("")
     else:
         print(g_results["links"][i])
         print(y_results ["links"][i])
+        
     if i == 5:
         break
