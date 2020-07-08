5ab0f863d7e60f12bb4a3885f09656a03d433007
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-07 22:31:16 -0400

    Update SearchEngineScraper.py

--- SearchEngineScraper.py
+++ SearchEngineScraper.py
@@ -1,26 +1,30 @@
-import pprint
-from search_engine_parser.core.engines.bing import Search as BingSearch
-from search_engine_parser.core.engines.google import Search as GoogleSearch
 from search_engine_parser.core.engines.yahoo import Search as YahooSearch
+from search_engine_parser.core.engines.google import Search as GoogleSearch
+from search_engine_parser.core.engines.bing import Search as BingSearch
 from search_engine_parser.core.engines.duckduckgo import Search as DuckSearch
 
-#Search set
-search = ('preach to the choir', 1)
-
-#Desired search engines
+#Intializing Search Engines
 g_search = GoogleSearch()
 y_search = YahooSearch()
 b_search = BingSearch()
 d_search = DuckSearch()
 
-#Result variables
-g_results = g_search.search(*search)
-y_results = y_search.search(*search)
-b_results = b_search.search(*search)
-d_results = d_search.search(*search)
-a = {
-    "Bing": b_results,
-    "DuckDuckGo": d_results,
-    "Google": g_results,
-    "Yahoo": y_results
-    }
+#Search prompt
+search_query = input("Enter your search query here: ") 
+search_set = (search_query, 1)
+
+#Searching for query
+g_results = g_search.search(*search_set)
+#b_results = b_search.search(*search_set)
+#d_results = d_search.search(*search_set)
+y_results = y_search.search(*search_set)
+
+# print first title from google search
+for i in range(0,5):
+    if g_results["links"] == y_results["links"]:
+       print("")
+    else:
+        print(g_results["links"][i])
+        print(y_results ["links"][i])
+    if i == 5:
+        break
