26be72c7d8c84de06d8009a2035cbcfb96bcbbb2
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-08 21:01:27 -0400

    Update SearchEngineScraper.py

--- SearchEngineScraper.py
+++ SearchEngineScraper.py
@@ -20,6 +20,13 @@ b_results = b_search.search(*search_set2)
 d_results = d_search.search(*search_set2)
 y_results = y_search.search(*search_set)
 
+#Create csv file
+with open('mycsv.csv','w', newline = '') as file:
+    #Creating writer
+    writer = csv.writer(file)
+    #Writing each row's header
+    writer.writerow(['Bing','DuckDuckGo','Google','Yahoo'])
+    
 # print first 10 links from each search engine
 for i in range(0,11):
     if g_results["links"] == y_results["links"] and g_results["links"] == d_results["links"] and g_results["links"] == b_results["links"] :
@@ -33,5 +40,8 @@ for i in range(0,11):
         print(y_results ["links"][i])
 
     print(g_results["links"][i])
-    
+      #Writing each row
+    writer.writerow([b_results["links"][i], d_results["links"][i], g_results["links"][i], y_results ["links"][i] ])
+    if i==11:
+        file.close()
   
