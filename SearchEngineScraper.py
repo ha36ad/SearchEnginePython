05f09f80d3efc6dad07c41e3e242e1650dc30c2a
Hammad Khan <59400114+ha36ad@users.noreply.github.com>
2020-07-08 20:17:07 -0400

    Update SearchEngineScraper.py

--- SearchEngineScraper.py
+++ SearchEngineScraper.py
@@ -21,14 +21,17 @@ d_results = d_search.search(*search_set2)
 y_results = y_search.search(*search_set)
 
 # print first 10 links from each search engine
-for i in range(0,10):
-    if g_results["links"] == y_results["links"]:
-        print(b_results["links"][i])
-        print(d_results ["links"][i])
-        print("")
+for i in range(0,11):
+    if g_results["links"] == y_results["links"] and g_results["links"] == d_results["links"] and g_results["links"] == b_results["links"] :
+        print(b_results["links"][i+2])
+        print(d_results ["links"][i+1])
+        print(y_results ["links"][i+3])
+        
     else:
-        print(g_results["links"][i])
+        print(b_results["links"][i])
+        print(d_results["links"][i])
         print(y_results ["links"][i])
-    print(d_results["links"][i])
-    print(b_results["links"][i])    
+
+    print(g_results["links"][i])
+    
   
